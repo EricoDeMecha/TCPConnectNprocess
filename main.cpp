99175575175e21c035155b5a9f320d6e1071f4c2
EricoDeMecha <ford.koech.eric@gmail.com>
2021-11-27 06:56:43 -0500

    decrypted: The best performance improvement is the transition from the nonworking state to the working state

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <boost/regex.hpp>
-
+#include <boost/algorithm/hex.hpp>
+#include "gzip.h"
 
 int main(int argc, char* argv[])
 {
@@ -16,14 +17,29 @@ int main(int argc, char* argv[])
         std::cout << "Connection successful" << '\n';
         // read from the connected socket
         for(;;){
-            boost::array<char, 128> buf{};
+            boost::array<char, 1024> buf{};
             boost::system::error_code ec;
             size_t len = sock.read_some(boost::asio::buffer(buf), ec);
             if(ec == boost::asio::error::eof)
                 break;
             else if(ec)
                 throw boost::system::system_error(ec);
-            std::cout.write(buf.data(), len);
+            std::stringstream message_stream;
+            message_stream.write(buf.data(), len);
+            std::string message  = message_stream.str();
+            std::cout << message << '\n';
+            boost::regex expr{"^(0x|0X)?[a-fA-F0-9]+$"};
+            boost::smatch match;
+            if(boost::regex_search(message, match, expr)){
+                std::cout << "This is what is found" <<'\n';
+                std::string m_str  = match[0];
+                std::cout << m_str << '\n';
+                std::string o_str;
+                boost::algorithm::unhex(m_str, std::back_inserter(o_str));
+                std::string f_str = Gzip::decompress(o_str);
+                std::cout << f_str << '\n';
+                break;
+            }
         }
     }catch(boost::system::system_error& e){
         std::cout << "Error occured!" << \
--- test.cpp
+++ test.cpp
@@ -1,10 +1,13 @@
-#include <boost/regex.hpp>
-#include <string>
 #include <iostream>
+#include <boost/algorithm/hex.hpp>
+#include "gzip.h"
 
 
 int main(){
-    std::string s = "1f8b080032f7d85f02ff55cac10980400c04c056b602abb18153721a249b2309dabe873fbfc3aca760932c0c89ee618dbb406d84df62c282266a9e8ac6d452277ab87d46e7e371290f64b59ac93fffe1f202fcefa41e62000000\n";
-    boost::regex expr{"^(0x|0X)?[a-fA-F0-9]+$\n"};
-    std::cout << std::boolalpha << boost::regex_match(s, expr) << '\n';
+    std::string s = "1f8b080032f7d85f02ff55cac10980400c04c056b602abb18153721a249b2309dabe873fbfc3aca760932c0c89ee618dbb406d84df62c282266a9e8ac6d452277ab87d46e7e371290f64b59ac93fffe1f202fcefa41e62000000";
+    std::string o_str;
+    boost::algorithm::unhex(s, std::back_inserter(o_str));
+    std::string io_str = Gzip::decompress(o_str);
+    std::cout << io_str << '\n';
+    return 0;
 }
