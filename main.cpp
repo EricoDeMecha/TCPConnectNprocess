773ef33bf25c748b72f82948a41c1024b158c7ed
EricoDeMecha <ford.koech.eric@gmail.com>
2021-11-27 05:13:26 -0500

    Read Successfull

--- main.cpp
+++ main.cpp
@@ -3,18 +3,7 @@
 #include <boost/array.hpp>
 #include <boost/regex.hpp>
 
-boost::asio::streambuf receive_buffer;
-void handler(const boost::system::error_code& e, std::size_t size)
-{
-    std::cout << "reached here" << std::endl;
-    if (!e)
-    {
-        std::istream message_stream(&receive_buffer);
-        std::string line;
-        std::getline(message_stream, line);
-        std::cout << line  << '\n';
-    }
-}
+
 int main(int argc, char* argv[])
 {
     std::string raw_ip_addr  = "20.108.244.219";
@@ -26,8 +15,16 @@ int main(int argc, char* argv[])
         sock.connect(ep);
         std::cout << "Connection successful" << '\n';
         // read from the connected socket
-        boost::system::error_code ec;
-        boost::asio::async_read_until(sock, receive_buffer,boost::regex("(?<=1)(.*)(?=)"), handler);
+        for(;;){
+            boost::array<char, 128> buf{};
+            boost::system::error_code ec;
+            size_t len = sock.read_some(boost::asio::buffer(buf), ec);
+            if(ec == boost::asio::error::eof)
+                break;
+            else if(ec)
+                throw boost::system::system_error(ec);
+            std::cout.write(buf.data(), len);
+        }
     }catch(boost::system::system_error& e){
         std::cout << "Error occured!" << \
         "Error code: "<<e.code() << \
